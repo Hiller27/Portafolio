fd8fa13c3c27d0a39f8cd86baac15b9b671338f1
HillerFullstack <159482459+KAREN-FULL4@users.noreply.github.com>
2025-01-28 14:05:40 -0500

    projects

--- src/app/completed-projects/completed-projects.component.ts
+++ src/app/completed-projects/completed-projects.component.ts
@@ -17,5 +17,20 @@ export class CompletedProjectsComponent {
     imageUrl: 'https://lirp.cdn-website.com/f167db5d/dms3rep/multi/opt/SERVIMAX+LOGO+NUEVO+ALTO+RELIEVE+HORIZONTAL-1920w.png',
     alt: 'Proyecto 1'
   },
+  {
+    name: 'Servimax',
+    phone: '317 3696744',
+    website: 'https://servimax.co/#/dashboard',
+    imageUrl: 'https://www.valoraanalitik.com/wp-content/uploads/2023/03/Mercado-Libre-1-696x406.jpg',
+    alt: 'Proyecto 1'
+  },
+  {
+    name: 'Servimax',
+    phone: '317 3696744',
+    website: 'https://servimax.co/#/dashboard',
+    imageUrl: 'https://lirp.cdn-website.com/f167db5d/dms3rep/multi/opt/SERVIMAX+LOGO+NUEVO+ALTO+RELIEVE+HORIZONTAL-1920w.png',
+    alt: 'Proyecto 1'
+  },
+
 ];
 }
